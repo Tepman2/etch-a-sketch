b2bd19c3d925308dd7e367a301e4efc8ec9be087
Seth Lienemann <sethlienemann@gmail.com>
2014-08-23 16:23:26 -0500

    Add pin operations file.

---
+++ src/hw_pin_ops.c
@@ -0,0 +1,17 @@
+/********************************************************************
+*
+*       Filename:  hw_pin_ops.c
+*
+*    Description:  This file defines the pin operation
+*
+*        Version:  1.0
+*        Created:  08/17/2014 09:45:03 AM
+*       Revision:  none
+*       Compiler:  gcc
+*
+*         Author:  Seth Lienemann (sal), sethlienemann@gmail.com
+*   Organization:
+*
+*******************************************************************/
+#include <stdio.h>
+
